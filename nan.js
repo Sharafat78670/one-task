096381720e89747f074a730515f87aad1791cdfc
Sharafat Karim <60249869+Sharafat78670@users.noreply.github.com>
2024-01-26 19:17:44 +0600

    Added remainder teask

---
+++ nan.js
@@ -0,0 +1,4 @@
+var a = isNaN("sh");
+
+// var a = isNaN(2-10);
+console.log(a)
---
+++ remainder.js
@@ -0,0 +1,7 @@
+var given_number=119;
+var divider_number=5;
+var remainder= given_number % divider_number;
+
+document.getElementById("given-num").innerHTML="Given number is " + given_number + "."
+document.getElementById("divided").innerHTML="Divider number is " + divider_number + "."
+document.getElementById("remainder").innerHTML="Remainder is " + remainder + "."
