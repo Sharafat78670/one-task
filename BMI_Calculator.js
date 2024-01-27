09d04c8fa0b64400c0497eb4d50589e9c3bac4af
Sharafat Karim <60249869+Sharafat78670@users.noreply.github.com>
2024-01-27 15:36:26 +0600

    Added BMI

---
+++ BMI_Calculator.js
@@ -0,0 +1,17 @@
+var weight= 105;
+var hight= 1.8;
+
+var BMI = weight / (hight)^2;
+
+if (BMI < 18.5) {
+    console.log("You are underweight.")
+} 
+else if (18.5 <= BMI >= 24.9) {
+    console.log("You are normal.") 
+} 
+else if (25 <= BMI >= 29.9) {
+    console.log("You are overweight.") 
+} 
+else{
+    console.log("You are obese.") 
+}
