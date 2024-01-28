bfb9790a2b67a3042b5c70af13f0c9bd4226c88d
Sharafat Karim <60249869+Sharafat78670@users.noreply.github.com>
2024-01-28 16:22:06 +0600

    Added Array chick file

---
+++ Array-notArray.js
@@ -0,0 +1,14 @@
+var one=['Sharafat'];
+var two='[]';
+var three= ['Sharafat','Karim','Shuvo'];
+
+console.log("Variable : " ,two);
+var chick=Array.isArray(two);
+
+if (chick == true) {
+    console.log("This variable is Array .")
+} 
+else {
+    console.log("This variable is not Array .")
+
+}
