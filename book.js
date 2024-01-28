c30af30824f017fc8964752229016ace30ba1b6b
Sharafat Karim <60249869+Sharafat78670@users.noreply.github.com>
2024-01-28 16:02:42 +0600

    Added book js file

---
+++ book.js
@@ -0,0 +1,17 @@
+const book=['Bangla','English','Math','Science','Programming'];
+
+console.log("The book list : " + book)
+
+var chick = 'Math';
+
+
+if (book.includes(chick)) {
+    console.log("Chick book : "+chick )
+    console.log("The element is present in the book list.")
+}
+ else {
+    console.log("Chick book : "+chick )
+    console.log("The element is absent in the book list.")
+
+}
+
